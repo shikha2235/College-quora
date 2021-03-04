2baab25e12be03af8b1796bc4e8f10aac6b79b31
shikha2235 <72398116+shikha2235@users.noreply.github.com>
2021-03-04 11:58:12 +0530

    Add files via upload

---
+++ react-slack-clone/src/actions/types.js
@@ -0,0 +1,13 @@
+
+
+export const SET_USER= 'SET_USER';
+export const CLEAR_USER='CLEAR_USER';
+
+// channel action type
+export const SET_CURRENT_CHANNEL ="SET_CURRENT_CHANNEL";
+export const SET_PRIVATE_CHANNEL ="SET_PRIVATE_CHANNEL";
+export const SET_USER_POSTS= "SET_USER_POSTS";
+
+// colors types 
+
+export const SET_COLORS='SET_COLORS';
